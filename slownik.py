e45c0312ef839fa70ad82a3d5bae4a09672b925c
KBARTKOW <kbartkow@users.noreply.github.com>
2018-12-09 14:35:17 +0100

    Slownik

---
+++ slownik.py
@@ -0,0 +1,10 @@
+produkty = {'S1222': 'sukienka trojkat',
+            'P1222': 'spodnie krata',
+            'X212': 'konsola do gier'}
+igla = 'X2X'
+
+if igla in produkty:
+    print("Znalazlem {0}.format(igla))")
+else:
+    print("Brak w magazynie {0}".format(igla))
+    
